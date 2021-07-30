1da753186b9e63443158becc0c7bb84caa0dff27
Daniel Nolan <daniel.nolan@edenred.com>
2021-07-30 14:59:23 +0100

    fixed resposivity of status message

--- script.js
+++ script.js
@@ -91,36 +91,31 @@ inputs.forEach((input) => {
 function validateForm() {
   var name = document.getElementById('name').value;
   if (name == '') {
-    document.querySelector('#status').innerHTML =
-      '<strong>Name cannot be empty</strong>';
+    document.querySelector('#status').innerHTML = 'Name cannot be empty';
     return false;
   }
   var email = document.getElementById('email').value;
   if (email == '') {
-    document.querySelector('#status').innerHTML =
-      '<strong>Email cannot be empty</strong>';
+    document.querySelector('#status').innerHTML = 'Email cannot be empty';
     return false;
   } else {
     var re =
       /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<i>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (!re.test(email)) {
-      document.querySelector('#status').innerHTML =
-        '<strong>Email format invalid</strong>';
+      document.querySelector('#status').innerHTML = 'Email format invalid';
       return false;
     }
   }
   var subject = document.getElementById('subject').value;
   if (subject == '') {
-    document.querySelector('#status').innerHTML =
-      '<strong>Subject cannot be empty</strong>';
+    document.querySelector('#status').innerHTML = 'Subject cannot be empty';
     return false;
   }
   var message = document.getElementById('message').value;
   if (message == '') {
-    document.querySelector('#status').innerHTML =
-      '<strong>Message cannot be empty</strong>';
+    document.querySelector('#status').innerHTML = 'Message cannot be empty';
     return false;
   }
 
-  document.getElementById('status').innerHTML = '<strong>Sending...</strong>';
+  document.getElementById('status').innerHTML = 'Sending...';
 }
